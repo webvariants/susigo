bdc213154ac24cbfa6b0f9095f36b927bb335dde
Tino Rusch <tino.rusch@webvariants.de>
2015-08-26 11:04:34 +0200

    added unregister[Consumer|Processor];

--- susigo.go
+++ susigo.go
@@ -3,6 +3,7 @@ package susigo
 import (
     "crypto/tls"
     "encoding/json"
+    "errors"
     "log"
     "net"
     "strconv"
@@ -16,8 +17,8 @@ type Susi struct {
     encoder    *json.Encoder
     decoder    *json.Decoder
     callbacks  map[string]Callback
-    consumers  map[string][]Callback
-    processors map[string][]Callback
+    consumers  map[string]map[int64]Callback
+    processors map[string]map[int64]Callback
 }
 
 func NewSusi(addr, certFile, keyFile string) (*Susi, error) {
@@ -35,8 +36,8 @@ func NewSusi(addr, certFile, keyFile string) (*Susi, error) {
     }
     susi.conn = conn
     susi.callbacks = make(map[string]Callback)
-    susi.consumers = make(map[string][]Callback)
-    susi.processors = make(map[string][]Callback)
+    susi.consumers = make(map[string]map[int64]Callback)
+    susi.processors = make(map[string]map[int64]Callback)
     susi.encoder = json.NewEncoder(susi.conn)
     susi.decoder = json.NewDecoder(susi.conn)
     go susi.backend()
@@ -57,32 +58,82 @@ func (susi *Susi) Publish(topic string, payload interface{}, callback Callback) 
     return susi.encoder.Encode(packet)
 }
 
-func (susi *Susi) RegisterConsumer(topic string, callback Callback) error {
-    susi.consumers[topic] = append(susi.consumers[topic], callback)
-    if len(susi.consumers[topic]) == 1 {
+func (susi *Susi) RegisterConsumer(topic string, callback Callback) (int64, error) {
+    if susi.consumers[topic] == nil {
+        susi.consumers[topic] = make(map[int64]Callback)
+    }
+    consumers := susi.consumers[topic]
+    id := time.Now().UnixNano()
+    consumers[id] = callback
+    if len(consumers) == 1 {
         packet := map[string]interface{}{
             "type": "registerConsumer",
             "data": map[string]interface{}{
                 "topic": topic,
             },
         }
-        return susi.encoder.Encode(packet)
+        return id, susi.encoder.Encode(packet)
     }
-    return nil
+    return -1, nil
 }
 
-func (susi *Susi) RegisterProcessor(topic string, callback Callback) error {
-    susi.processors[topic] = append(susi.processors[topic], callback)
-    if len(susi.processors[topic]) == 1 {
+func (susi *Susi) RegisterProcessor(topic string, callback Callback) (int64, error) {
+    if susi.processors[topic] == nil {
+        susi.processors[topic] = make(map[int64]Callback)
+    }
+    processors := susi.processors[topic]
+    id := time.Now().UnixNano()
+    processors[id] = callback
+    if len(processors) == 1 {
         packet := map[string]interface{}{
             "type": "registerProcessor",
             "data": map[string]interface{}{
                 "topic": topic,
             },
         }
-        return susi.encoder.Encode(packet)
+        return id, susi.encoder.Encode(packet)
     }
-    return nil
+    return -1, nil
+}
+
+func (susi *Susi) UnregisterConsumer(id int64) error {
+    for _, consumers := range susi.consumers {
+        if topic, ok := consumers[id]; ok {
+            delete(consumers, id)
+            if len(consumers) == 0 {
+                packet := map[string]interface{}{
+                    "type": "unregisterConsumer",
+                    "data": map[string]interface{}{
+                        "topic": topic,
+                    },
+                }
+                return susi.encoder.Encode(packet)
+            } else {
+                return nil
+            }
+        }
+    }
+    return errors.New("no such consumer")
+}
+
+func (susi *Susi) UnregisterProcessor(id int64) error {
+    for _, processors := range susi.processors {
+        if topic, ok := processors[id]; ok {
+            delete(processors, id)
+            if len(processors) == 0 {
+                packet := map[string]interface{}{
+                    "type": "unregisterProcessor",
+                    "data": map[string]interface{}{
+                        "topic": topic,
+                    },
+                }
+                return susi.encoder.Encode(packet)
+            } else {
+                return nil
+            }
+        }
+    }
+    return errors.New("no such processor")
 }
 
 func (susi *Susi) backend() {
